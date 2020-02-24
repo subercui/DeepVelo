595f3830c7229434bfbc5b2e398c48cb38603e47
subercui <subercui@gmail.com>
2020-02-23 23:49:02 -0500

    [update] add cell average projection

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -3,6 +3,7 @@ from base import BaseDataLoader
 import torch
 from torch.utils.data import Dataset
 import numpy as np
+from sklearn.metrics import pairwise_distances
 
 class VeloDataset(Dataset):
     def __init__(self, data_dir, train=True):
@@ -11,6 +12,13 @@ class VeloDataset(Dataset):
         self.Sx_sz = data_obj['Sx_sz'].T
         self.velo = data_obj['velo'].T # shape (1448, 1720) to shape (1720, 1448)
         
+
+        self.S_tp1 = self.Sx_sz + self.velo
+        dist = pairwise_distances(self.S_tp1, self.Sx_sz)  # (1720, 1720)
+        ind = np.argsort(dist, axis=1)[:, :30]
+        for i in range(self.velo.shape[0]):
+            self.velo[i] = np.mean(self.Sx_sz[ind[i]], axis=0) - self.Sx_sz[i]
+
         self.Ux_sz = torch.tensor(self.Ux_sz, dtype=torch.float32)
         self.Sx_sz = torch.tensor(self.Sx_sz, dtype=torch.float32)
         self.velo = torch.tensor(self.velo, dtype=torch.float32)
@@ -20,6 +28,7 @@ class VeloDataset(Dataset):
         return len(self.Ux_sz)  # 1720
 
     def __getitem__(self, i):
+        
         data_dict = {
             "Ux_sz": self.Ux_sz[i],
             "Sx_sz": self.Sx_sz[i],
