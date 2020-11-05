d3d6541326943ad173775d678f291ead85afd1dc
subercui <subercui@gmail.com>
2020-11-04 23:59:46 -0500

    [update] adaptive loss training

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -145,3 +145,57 @@ class Trainer(BaseTrainer):
             current = batch_idx
             total = self.len_epoch
         return base.format(current, total, 100.0 * current / total)
+
+
+class AdaptiveTrainer(Trainer):
+    """
+    Adaptive Trainer for adaptive training stage. Target is computed every epoch.
+    """
+    
+    def _train_epoch(self, epoch):
+        """
+        Training logic for an epoch
+
+        :param epoch: Integer, current training epoch.
+        :return: A log that contains average loss and metric in this epoch.
+        """
+        self.model.train()
+        self.train_metrics.reset()
+        for batch_idx, batch_data in enumerate(self.data_loader):
+            output, target = self._compute_core(batch_data)
+            if self.config['mask_zeros']:
+                data_dict = batch_data
+                mask = data_dict['mask'].to(self.device)  # (batch, n_gene)
+                output = output*mask
+                target = target*mask
+            self.optimizer.zero_grad()
+            loss = self.criterion(output, target)
+            loss.backward()
+            self.optimizer.step()
+
+            if self.config['constraint_loss']:
+                self._smooth_constraint_step()
+            
+            self.writer.set_step((epoch - 1) * self.len_epoch + batch_idx)
+            self.train_metrics.update('loss', loss.item())
+            for met in self.metric_ftns:
+                self.train_metrics.update(met.__name__, met(output, target))
+
+            if batch_idx % self.log_step == 0:
+                self.logger.debug('Train Epoch: {} {} Loss: {:.6f}'.format(
+                    epoch,
+                    self._progress(batch_idx),
+                    loss.item()))
+                # self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
+
+            if batch_idx == self.len_epoch:
+                break
+        log = self.train_metrics.result()
+
+        if self.do_validation:
+            val_log = self._valid_epoch(epoch)
+            log.update(**{'val_'+k : v for k, v in val_log.items()})
+
+        if self.lr_scheduler is not None:
+            self.lr_scheduler.step()
+        return log
