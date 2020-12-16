e4f264c35634568b6e8dcc1a010a1685539337db
subercui <subercui@gmail.com>
2020-12-16 09:55:40 -0500

    [update] reformat scvelo_test

--- scvelo_test.py
+++ scvelo_test.py
@@ -14,7 +14,8 @@ MASK_ZERO = True
 DEEPVELO = True  # choice of {True, False, 'ShowTarget'}
 DYNAMICAL = False  # whether use the dynamical mode of scvelo and compute latent time
 DEEPVELO_FILE = 'scvelo_mat.npz'
-data = 'EP'  # choice of {'EP', 'DG', 'velocyto_dg', 'velocyto_hg', 'E9M2_Glial', 'E9-11F1_Glial', 'E9-11M2_Glial', 'E9-11F1_Gluta'}
+# choice of {'EP', 'DG', 'velocyto_dg', 'velocyto_hg', 'E9M2_Glial', 'E9-11F1_Glial', 'E9-11M2_Glial', 'E9-11F1_Gluta'}
+data = 'EP'
 SURFIX = '[dynamical]' if DYNAMICAL else ''
 SURFIX += '[deep_velo]' if DEEPVELO else ''
 
@@ -26,17 +27,21 @@ elif data == 'EP':
     adata = scv.datasets.pancreas()
 elif data == 'velocyto_dg':
     adata = scv.read('data/DentateGyrus.loom', cache=True)
-    adata.obsm['tsne'] = adata.obs[['TSNE1','TSNE2']].to_numpy()
+    adata.obsm['tsne'] = adata.obs[['TSNE1', 'TSNE2']].to_numpy()
 elif data == 'velocyto_hg':
     adata = scv.read('data/hgForebrainGlut.loom', cache=True)
 elif data == 'E9M2_Glial':
-    adata = scv.read('data/E9M2_glial.h5ad',cache=True)
+    adata = scv.read('data/E9M2_glial.h5ad', cache=True)
 elif data == 'E9-11F1_Glial':
     adata = scv.read('data/E9-11F1_glial.h5ad', cache=True)
 elif data == 'E9-11M2_Glial':
     adata = scv.read('data/E9-11M2_glial.h5ad', cache=True)
 elif data == 'E9-11F1_Gluta':
     adata = scv.read('data/E9-11F1_glutamergic.h5ad', cache=True)
+else:
+    raise ValueError(
+        "choose data from \{'EP', 'DG', 'velocyto_dg', 'velocyto_hg',"
+        "'E9M2_Glial', 'E9-11F1_Glial', 'E9-11M2_Glial', 'E9-11F1_Gluta'\}")
 
 
 # %% Preprocessing Data
@@ -57,11 +62,11 @@ else:
 
 # %% output and change the velocity
 to_save = {
-    'Ux_sz':adata.layers['Mu'].T, 
-    'Sx_sz':adata.layers['Ms'].T, 
-    'velo':adata.layers['velocity'].T,
-    'conn':adata.obsp['connectivities'].T  # (features, cells)
-    } # have to input in dimmention order (1999 genes, 2930 cells)
+    'Ux_sz': adata.layers['Mu'].T,
+    'Sx_sz': adata.layers['Ms'].T,
+    'velo': adata.layers['velocity'].T,
+    'conn': adata.obsp['connectivities'].T  # (features, cells)
+}  # have to input in dimmention order (1999 genes, 2930 cells)
 with open('./data/scveloDG.npz', 'wb') as f:
     pickle.dump(to_save, f)
 if DEEPVELO == 'ShowTarget':
@@ -83,7 +88,8 @@ elif DEEPVELO:
     # if using test
     # os.system(f'python train.py -c config_test.json --ng {n_genes} --ot {DEEPVELO_FILE} --dd ./data/scveloDG.npz')
     # os.system(f'python train.py -c config_test_gin.json --ng {n_genes} --bs {batch_size} --ot {DEEPVELO_FILE} --dd ./data/scveloDG.npz')
-    os.system(f'python train.py -c config_test_gcn.json --ng {n_genes} --bs {batch_size} --ot {DEEPVELO_FILE} --dd ./data/scveloDG.npz')
+    os.system(
+        f'python train.py -c config_test_gcn.json --ng {n_genes} --bs {batch_size} --ot {DEEPVELO_FILE} --dd ./data/scveloDG.npz')
     print(f'finished in {time()-now:.2f}s')
 
     # load
@@ -99,18 +105,23 @@ if not ('X_umap' in adata.obsm or 'tsne' in adata.obsm):
 
 # %% plot
 if data == 'DG':
-    scv.pl.velocity_embedding_stream(adata, basis='umap', color='clusters', dpi=300, save=f'velo_emb_stream{SURFIX}.png')
+    scv.pl.velocity_embedding_stream(adata, basis='umap', color='clusters',
+                                     dpi=300, save=f'velo_emb_stream{SURFIX}.png')
     # scv.pl.velocity_embedding(adata, basis='umap', arrow_length=1.2, arrow_size=1.2, dpi=150)
-    scv.pl.velocity_embedding_grid(adata, basis='umap', arrow_length=1.2, arrow_size=1.2, dpi=300, save=f'velo_emb_grid{SURFIX}.png')
+    scv.pl.velocity_embedding_grid(adata, basis='umap', arrow_length=1.2, arrow_size=1.2,
+                                   dpi=300, save=f'velo_emb_grid{SURFIX}.png')
 elif data == 'EP':
     scv.pl.velocity_embedding_stream(adata, basis='umap', dpi=300, save=f'velo_emb_stream{SURFIX}.png')
 elif data.startswith('E9'):
     # scv.pl.velocity_embedding_stream(adata, basis='umap', dpi=300, title=f'{data}', save=f'{data}velo_stream{SURFIX}.png')
-    scv.pl.velocity_embedding_stream(adata, basis='pca', color='time', dpi=300, title=f'{data}_pca', save=f'{data}velo_stream{SURFIX}[pca].png')
+    scv.pl.velocity_embedding_stream(adata, basis='pca', color='time', dpi=300,
+                                     title=f'{data}_pca', save=f'{data}velo_stream{SURFIX}[pca].png')
 elif data == 'velocyto_hg':
-    scv.pl.velocity_embedding_stream(adata, basis='pca', color='Clusters', dpi=300, title=f'{data}_pca', save=f'{data}velo_stream{SURFIX}[pca].png')
+    scv.pl.velocity_embedding_stream(adata, basis='pca', color='Clusters', dpi=300,
+                                     title=f'{data}_pca', save=f'{data}velo_stream{SURFIX}[pca].png')
 elif data == 'velocyto_dg':
-    scv.pl.velocity_embedding_stream(adata, basis='tsne', color='Clusters', dpi=300, save=f'{data}velo_stream{SURFIX}[tsne].png')
+    scv.pl.velocity_embedding_stream(adata, basis='tsne', color='Clusters', dpi=300,
+                                     save=f'{data}velo_stream{SURFIX}[tsne].png')
 # %% more plots
 # scv.pl.velocity_graph(adata, dpi=300, save='velo_graph.pdf')
 if data == 'EP':
@@ -123,9 +134,9 @@ elif data.startswith('E9'):
 
 latent_time(adata)
 scv.pl.scatter(
-    adata, 
-    color='latent_time', 
-    color_map='gnuplot', 
+    adata,
+    color='latent_time',
+    color_map='gnuplot',
     size=80,
     dpi=300,
     save=f'latent_time{SURFIX}.png'
@@ -150,6 +161,7 @@ if data == 'DG':
 
     # metric 1: relative variance of cosine similarity within cluster
     from sklearn import preprocessing
+
     def metric1(data):
         norm_data = preprocessing.normalize(data, axis=1)
         var_cos = (norm_data@norm_data.T).var()
